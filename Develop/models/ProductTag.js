41f63aa0ce90c3d17ce71436a0a7c34c43c4e40f
gbroadhead <gwbroadhead@gmail.com>
2021-12-20 21:17:22 -0800

    tag is done

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -13,6 +13,15 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true
     },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: { model: 'Product' key:'id'}
+    },
+
+    tag_id:{
+      type:DataTypes.INTEGER,
+      references: {model:'Tag' key:'id'}
+    }
   },
   {
     sequelize,
