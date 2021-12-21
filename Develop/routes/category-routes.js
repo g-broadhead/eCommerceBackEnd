1851bae97849c21b4ada590ac5112f80e611a702
gbroadhead <gwbroadhead@gmail.com>
2021-12-20 22:11:07 -0800

    category routes completed

--- Develop/routes/category-routes.js
+++ Develop/routes/category-routes.js
@@ -3,26 +3,35 @@ const { Category, Product } = require('../models')
 
 // The `/api/categories` endpoint
 
-router.get('/categories', (req, res) => {
+router.get('/categories', async function (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const categories = await Category.findAll({ include: [Product] })
+  res.json(categories)
 })
 
-router.get('/categories/:id', (req, res) => {
+router.get('/categories/:id', async function (({ params: { id } }, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const categories = await Category.findOne({ where: { id }, include: [Product] })
+  res.json(categories)
 })
 
-router.post('/categories', (req, res) => {
+router.post('/categories', async function (req, res) => {
   // create a new category
+  const categories = await Category.create(req.body)
+  res.status(200).json(categories)
 })
 
-router.put('/categories/:id', (req, res) => {
+router.put('/categories/:id', async function ({ params: { id } }, res) => {
   // update a category by its `id` value
+  const categories = await Category.update(req.body, { where: { id } })
 })
 
-router.delete('/categories/:id', (req, res) => {
+router.delete('/categories/:id', async function ({ params: { id } }, res) => {
   // delete a category by its `id` value
+  const categories = await Category.destroy({ where: { id } })
+  res.status(200).json(categories)
 })
 
 module.exports = router
