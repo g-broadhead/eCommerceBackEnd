9dcb4565868659305d787973a3315b9a3627e897
gbroadhead <gwbroadhead@gmail.com>
2021-12-21 23:17:04 -0800

    bugs fixed

--- routes/tag-routes.js
+++ routes/tag-routes.js
@@ -6,14 +6,14 @@ const { Tag, Product, ProductTag } = require('../models')
 router.get('/tags', async function (req, res) {
   // find all tags
   // be sure to include its associated Product data
-  const tags = await Tag.findAll(req.body)
+  const tags = await Tag.findAll({ include: [Product] })
   res.json(tags)
 })
 
 router.get('/tags/:id', async function ({ params: { id } }, res) {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  const tags = await Tag.findOne(req.body)
+  const tags = await Tag.findOne({ where: { id }, include: [Product]})
   res.json(tags)
 })
 
@@ -25,7 +25,7 @@ router.post('/tags', async function (req, res) {
 
 router.put('/tags/:id', async function (req, res) {
   // update a tag's name by its `id` value
-  const tags = await Tag.update(req.body, ({ where: { id } }))
+  const tags = await Tag.update(req.body, {where: { id: req.params.id }})
   res.status(200).json(tags)
 })
 
